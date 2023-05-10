1dc37f4080f7b35146c8cf6803d303ba832adc92
mercury640 <54633837+mercury640@users.noreply.github.com>
2023-05-10 15:05:46 -0400

    Delete python directory

--- python/cat.py
+++
@@ -1,8 +0,0 @@
-#Simplest app.py code for demo
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "<h1>Hello from Flask & Docker</h2>"
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=4000)
