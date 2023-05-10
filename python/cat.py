2916016c3f9b16c49ac8cb2cd23d4fb5e1f4070d
mercury640 <54633837+mercury640@users.noreply.github.com>
2023-05-10 15:02:15 -0400

    Update cat.py

--- python/cat.py
+++ python/cat.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return "<img src=https://cataas.com/cat>"
+    return "<h1>Hello from Flask & Docker</h2>"
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(host="0.0.0.0",port=4000)
